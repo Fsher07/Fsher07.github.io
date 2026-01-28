b0ce818080bf6196a85f889792dc713c54245e78
Furkan Salim Hıdır <furkansalimm@gmail.com>
2026-01-28 16:55:39 +0300

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/FrameWorks";
+import { Frameworks } from "../components/Frameworks";
 
 const About = () => {
   const grid2Container = useRef();
